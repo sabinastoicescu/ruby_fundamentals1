66aa57f8a79b07e437841ed4a808383141a5dfa8
Sabina <sabina_stoicescu@yahoo.com>
2015-03-31 16:10:23 -0400

    exercise 3 finished and added

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
 puts "What is your name?"
 name=gets.chomp
 puts "Hi #{name}!"
+puts "How old are you turning this year?"
+age=gets.chomp.to_i
+
+time=Time.now
+year=time.year
+puts "You were born in the year #{year - age}"
