07ef8a0ecdc0c525c4eae09634f88962ca997e6f
Sabina <sabina_stoicescu@yahoo.com>
2015-03-31 15:40:28 -0400

    commited exercise 3 partly

---
+++ exercise2.rb
@@ -0,0 +1,5 @@
+puts 55 * 0.1 # this is what i would leave as tip for a meal
+puts "good" + 1.to_s #adding string with an integer
+result = 45628 * 7839	
+puts "This is what came out by multiplying 45628 with 7839: #{result}"#using string interpolation to describe the result
+puts (true && false) || (false&&true)||!(false&&false)
